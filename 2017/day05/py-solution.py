f6cfaa1029319a92c54a52aa62a3d0b24a98806d
treetopbottle <treetopbottle@github>
2017-12-05 06:17:58 +0100

    day 5 solution 1

---
+++ 2017/day05/py-solution.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+import collections
+import re
+
+file_name = sys.argv[1]
+
+with open(file_name) as f:
+    jumps = f.read().strip()
+
+    jumps = [int(i) for i in jumps.split('\n')]
+
+    steps = 0
+    position = 0
+    next_pos = 0
+    try:
+        while True:
+            #print(jumps, steps)
+            next_pos += jumps[position]
+            jumps[position] += 1
+            position = next_pos
+            steps += 1
+    except IndexError:
+        pass
+        
+
+    print(steps)
+

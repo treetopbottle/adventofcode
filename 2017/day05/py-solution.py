6c79f46d648f7bfb4a483766650f48575d7f3075
treetopbottle <treetopbottle@github>
2017-12-05 06:18:31 +0100

    add day 5 solution 2

--- 2017/day05/py-solution.py
+++ 2017/day05/py-solution.py
@@ -18,7 +18,10 @@ with open(file_name) as f:
         while True:
             #print(jumps, steps)
             next_pos += jumps[position]
-            jumps[position] += 1
+            if jumps[position] >= 3:
+                jumps[position] -= 1
+            else:
+                jumps[position] += 1
             position = next_pos
             steps += 1
     except IndexError:

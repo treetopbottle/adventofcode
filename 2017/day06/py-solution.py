922af694bdb9f3428a8fbd8f5a808bd3d6f6a86a
treetopbottle <treetopbottle@github>
2017-12-06 07:57:56 +0100

    day 06 solution 2

--- 2017/day06/py-solution.py
+++ 2017/day06/py-solution.py
@@ -12,12 +12,14 @@ with open(file_name) as f:
     memory_banks = [int(i) for i in input_.split('\t')]
 
     seen_distributions = set()
+    wanted_distribution = []
 
     nr_cycles = 0
     while True:
         # Update seen ones
         distribution = str(memory_banks)
         if distribution in seen_distributions:
+            wanted_distribution = distribution
             break
         seen_distributions.add(distribution)
 
@@ -33,6 +35,32 @@ with open(file_name) as f:
             memory_banks[index] += 1
 
         nr_cycles += 1
+
+        print(memory_banks, nr_cycles)
+
+
+    # how long until second?
+    print('------------------')
+    print(wanted_distribution)
+
+    nr_cycles = 0
+    while True:
+        # Remove memory from max bank
+        index = memory_banks.index(max(memory_banks))
+        print(index)
+        nr_blocks = memory_banks[index]
+        memory_banks[index] = 0
+
+        # Redistribute memory
+        for i in range(1, nr_blocks+1):
+            index = (index+1) % len(memory_banks)
+            memory_banks[index] += 1
+
+        nr_cycles += 1
+
+        distribution = str(memory_banks)
+        if distribution == wanted_distribution:
+            break
 
         print(memory_banks, nr_cycles)
 

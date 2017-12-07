fad10e313c6d1f4057f2d309c7dfb2ac2bd5689c
treetopbottle <treetopbottle@github>
2017-12-07 08:08:35 +0100

    day 7 solution 2

--- 2017/day07/py-solution.py
+++ 2017/day07/py-solution.py
@@ -6,12 +6,20 @@ import re
 
 file_name = sys.argv[1]
 
+def get_weight(name, weights, towers):
+    w = weights[name]
+    if name in towers.keys():
+        for other in towers[name]:
+            w += get_weight(other, weights, towers)
+    return w
+
 with open(file_name) as f:
     input_ = f.readlines()
     lines = [i.strip() for i in input_]
 
     pointed_to = []
     program_weights = {}
+    towers = {}
     for program in lines:
         carries = None
         try:
@@ -20,16 +28,29 @@ with open(file_name) as f:
             pass
 
         name, weight = program.strip().split(' ')
-        weight = weight[1:-1]
+        weight = int(weight[1:-1])
         program_weights[name] = weight
 
         if carries:
             carries = carries.strip().split(',')
             carries = [i.strip() for i in carries]
-            pointed_to.extend(carries)
+            towers[name] = carries
+
+    total_weight = {}
+    for program_name in program_weights.keys():
+        total = get_weight(program_name, program_weights, towers)
+        total_weight[program_name] = total
 
 
-    print(pointed_to)
-    for p in program_weights.keys():
-        if p not in pointed_to:
-            print(p)
+    for name in total_weight.keys():
+        if name not in towers.keys():
+            continue
+        carries = towers[name]
+        weights = [total_weight[i] for i in carries]
+        if not len(set(weights)) == 1:
+            print(name, weights)
+            print(' ', towers[name])
+            print(' ', [total_weight[i] for i in towers[name]])
+    print([total_weight[i] for i in towers['ycbgx']])
+    print(program_weights['ycbgx'])
+

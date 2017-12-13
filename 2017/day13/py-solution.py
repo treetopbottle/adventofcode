ce7c674616253e983eb41289973fc4be37fc55ae
treetopbottle <treetopbottle@github>
2017-12-13 08:26:15 +0100

    day 13 solution 1 better version

--- 2017/day13/py-solution.py
+++ 2017/day13/py-solution.py
@@ -11,32 +11,18 @@ file_name = sys.argv[1]
 with open(file_name) as f:
     input_ = f.readlines()
 
-    end_layer = 0
-    #layers = collections.defaultdict(tuple)
     layers = {}
+    severity = 0
     for line in input_:
         layer, depth = [int(i) for i in line.strip().split(': ')]
 
+        steps_til_return = (depth - 1) * 2
+        if layer % steps_til_return == 0:
+            severity += layer * depth
+            print('caught in layer', layer, steps_til_return)
+            
+
         layers[layer] = (depth, 0, 1)
-        print(layer, depth)
-        end_layer = layer
-
-    severity = 0
-    for cur_layer in range(end_layer+1):
-        if cur_layer in layers and layers[cur_layer][1] == 0:
-            severity += cur_layer * layers[cur_layer][0]
-            print('caught in layer', cur_layer, severity)
-
-        for layer in layers:
-            depth, position, direction = layers[layer]
-            position += direction
-            if position == depth - 1:
-                direction = direction * -1
-            elif position == 0:
-                direction = direction * -1
-            layers[layer] = (depth, position, direction)
-        print(layers)
-        
 
     print(severity)
 

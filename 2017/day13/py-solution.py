d4dd528076bf6c2ae94fc948ad91c934813181d7
treetopbottle <treetopbottle@github>
2017-12-13 21:22:17 +0100

    day 13 solution 2 (very slow)

--- 2017/day13/py-solution.py
+++ 2017/day13/py-solution.py
@@ -22,7 +22,25 @@ with open(file_name) as f:
             print('caught in layer', layer, steps_til_return)
             
 
-        layers[layer] = (depth, 0, 1)
-
+        layers[layer] = depth
     print(severity)
+    print(layers)
+
+    for wait in range(10000000):
+        print('---')
+        caught = False
+        for layer in layers:
+            depth = layers[layer]
+
+            steps_til_return = (depth - 1) * 2
+            if (layer + wait) % steps_til_return == 0:
+                caught = True
+                print('caught in layer', layer, '( wait', wait, ')', steps_til_return)
+                break
+
+        if caught == False:
+            print('---')
+            print(wait)
+            break
+
 

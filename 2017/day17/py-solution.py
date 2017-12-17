99fcb97f4dab439e2db914c8bbbd119303839fb8
treetopbottle <treetopbottle@github>
2017-12-17 17:25:48 +0100

    day 17 solution 2 cleanup

--- 2017/day17/py-solution.py
+++ 2017/day17/py-solution.py
@@ -1,31 +1,18 @@
 #!/usr/bin/env python3
 
-import sys
-import collections
-import re
+step_size = 312
+#step_size = 3
 
-file_name = sys.argv[1]
+nr_steps = 50000000
+#nr_steps = 10
 
-with open(file_name) as f:
-    input_ = f.read().strip()
-    step_size = 312
-    #step_size = 3
+cur_index = 0
+number_after_zero = 0
+for num in range(1,nr_steps+1):
+    cur_index = ((cur_index + step_size) % num) + 1
 
-    nr_steps = 50000000
-    #nr_steps = 10
+    if cur_index == 1:
+        number_after_zero = num
 
-    buf = [0]
-    cur_index = 0
-    number_after_zero = 0
-    for num in range(1,nr_steps+1):
-        buf_size = num
-        cur_index = ((cur_index + step_size) % buf_size) + 1
-
-        if cur_index == 1:
-            number_after_zero = num
-            print('after', number_after_zero)
-
-
-    print('---')
-    print(number_after_zero)
+print(number_after_zero)
 

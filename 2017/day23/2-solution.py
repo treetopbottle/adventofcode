4eab5fd29745bb690ad145bb77789bf1c3e81286
treetopbottle <treetopbottle@github>
2017-12-23 09:45:45 +0100

    day 23 solution 2 rename files for clarity

--- 2017/day23/2-solution.py
+++ 2017/day23/2-solution.py
@@ -1,73 +1,20 @@
 #!/usr/bin/env python3
 
-import sys
-import collections
+# https://stackoverflow.com/questions/18833759/python-prime-number-checker/18833870#18833870
+import math
+def is_prime(n):
+    if n % 2 == 0 and n > 2: 
+        return False
+    return all(n % i for i in range(3, int(math.sqrt(n)) + 1, 2))
 
 
-def vis(instructions, cur_inst, registers):
-    for i in range(len(instructions)):
-        if i == cur_inst:
-            print('-> ', end='')
-        else:
-            print('   ', end='')
-        print(instructions[i], end='')
+b = 106500
+c = 123500
+step_size = 17
 
-    print()
+h = 0
+for b in range(b, c+step_size, step_size):
+    if not is_prime(b):
+        h += 1
 
-    for reg in registers:
-        print(reg, registers[reg])
-
-
-def get_number(registers, num):
-    if num in registers:
-        return registers[num]
-    return int(num)
-
-
-file_name = sys.argv[1]
-
-with open(file_name) as f:
-    instructions = f.readlines()
-
-    registers = collections.defaultdict(int)
-    registers['a'] = 1
-    registers['b'] = 0
-    registers['c'] = 0
-    registers['d'] = 0
-    registers['e'] = 0
-    registers['f'] = 0
-    registers['g'] = 0
-    registers['h'] = 0
-
-    times_mul_invoked = 0
-
-    done = False
-    i = 0
-    cntr = 1
-    while not done:
-        if i >= len(instructions) or i < 0:
-            break
-        instruction = instructions[i].strip()
-        op, *args = instruction.split(' ')
-        #print(op, args)
-        import time; time.sleep(0.1)
-        vis(instructions, i, registers)
-        if cntr % 1000 == 0:
-            pass
-
-        if op == 'set':
-            registers[args[0]] = get_number(registers, args[1])
-        elif op == 'sub':
-            registers[args[0]] -= get_number(registers, args[1])
-        elif op == 'mul':
-            registers[args[0]] *= get_number(registers, args[1])
-            times_mul_invoked += 1
-        elif op == 'jnz':
-            x = get_number(registers, args[0])
-            y = get_number(registers, args[1])
-            if x != 0:
-                i += y
-                i -= 1
-
-        i += 1
-        cntr += 1
+print(h)

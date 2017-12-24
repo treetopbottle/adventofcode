eb20379395ea35e808163d429afc930f5016f085
treetopbottle <treetopbottle@github>
2017-12-24 11:37:30 +0100

    day 24 solution 1 rewrite start

--- 2017/day24/1-solution.py
+++ 2017/day24/1-solution.py
@@ -5,24 +5,35 @@ import collections
 import copy
 
 
-def try_component(prev_comp, components, strength, strengths, components_left):
-    side1, side2 = prev_comp.split('/')
+def find_possible_bridges(bridge_so_far, all_components):
+    bridge_end = bridge_so_far[-1]
 
-    possibilities = set(components[side1] + components[side2])
+    n1, n2 = bridge_end.split('/')
 
-    for comp in possibilities:
-        if not comp in components_left:
+    # 0/x--0/y is illegal
+    if n1 == '0':
+        n1 = '-1'
+    
+    # Can only use ports once
+    if len(bridge_so_far) > 2:
+        cur_pins = bridge_so_far[-1].split('/')
+        prev_pins = bridge_so_far[-2].split('/')
+
+        for pin in prev_pins:
+            if pin in cur_pins:
+                cur_pins.remove(pin)
+        n1 = cur_pins[0]
+        if len(cur_pins) > 1:
+            n2 = cur_pins[1]
+
+    possible = set(all_components[n1] + all_components[n2])
+    for component in possible:
+        if component in bridge_so_far:
             continue
-
-        components_left_copy = copy.deepcopy(components_left)
-        components_left_copy.remove(comp)
-        side1, side2 = prev_comp.split('/')
-        new_strength = strength + int(side1) + int(side2)
-
-        try_component(comp, components, new_strength, strengths, components_left_copy)
-
-    strengths.add(strength)
-    print('recursion done', len(strengths))
+        new_bridge = bridge_so_far + [component]
+        find_possible_bridges(new_bridge, all_components)
+    
+    print(bridge_so_far[1:])
 
 
 file_name = sys.argv[1]
@@ -36,33 +47,12 @@ with open(file_name) as f:
         component = line.strip()
         what_is_left.append(component)
 
-        side1, side2 = line.strip().split('/')
+        side1, side2 = component.split('/')
         components[side1].append(component)
         components[side2].append(component)
 
     print(components)
 
-    strengths = set()
-    strength = 0
-    try_component('0/0', components, strength, strengths, what_is_left)
-
-    print(strengths)
-    print(max(strengths))
-
-    #comp_left = True
-    #sum_ = 0
-    #current = '0'
-    #while comp_left:
-    #    next_ = take_component(current, components)
-    #    print(next_)
-
-    #    if next_ == None:
-    #        comp_left = False
-    #        break
-
-    #    sum_ += int(next_)
-
-    #    current = next_
-
-    #print(sum_)
-
+    tried_combinations = set()
+    start_bridge = ['0/0']
+    find_possible_bridges(start_bridge, components)

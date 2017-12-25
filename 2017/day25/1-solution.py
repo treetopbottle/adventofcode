8fec3e8b2673e226872c86dfdacef20df7c167f3
treetopbottle <treetopbottle@github>
2017-12-25 06:43:02 +0100

    day 25 solution 1

--- 2017/day25/1-solution.py
+++ 2017/day25/1-solution.py
@@ -5,73 +5,40 @@ import collections
 import copy
 
 
-def state_A(tape, cursor):
-    if tape[cursor] == 0:
-        tape[cursor] = 1
-        return 'B', cursor + 1
-    else:
-        tape[cursor] = 0
-        return 'C', cursor - 1
+def parse(lines):
+    actions = {}
+    while len(lines) > 0:
+        _ = lines.pop(0)
+        state = lines.pop(0)[-3]
+        # first value
+        value = int(lines.pop(0)[-3])
+        new_value = int(lines.pop(0)[-3])
+        direction = lines.pop(0).split(' ')[-1][:-2]
+        if direction == 'right':
+            direction = 1
+        else:
+            direction = -1
+        new_state = lines.pop(0)[-3]
+        actions[(state, value)] = (new_value, direction, new_state)
+        # second value
+        value = int(lines.pop(0)[-3])
+        new_value = int(lines.pop(0)[-3])
+        direction = lines.pop(0).split(' ')[-1][:-2]
+        if direction == 'right':
+            direction = 1
+        else:
+            direction = -1
+        new_state = lines.pop(0)[-3]
+        actions[(state, value)] = (new_value, direction, new_state)
+    return actions
 
 
-def state_B(tape, cursor):
-    if tape[cursor] == 0:
-        tape[cursor] = 1
-        return 'A', cursor - 1
-    else:
-        tape[cursor] = 1
-        return 'A', cursor + 1
+def simulate(actions, state, tape, cursor):
+    current_value = tape[cursor]
+    new_value, offset, new_state = actions[(state, current_value)]
+    tape[cursor] = new_value
 
-
-def state_C(tape, cursor):
-    if tape[cursor] == 0:
-        tape[cursor] = 0
-        return 'B', cursor - 1
-    else:
-        tape[cursor] = 0
-        return 'E', cursor - 1
-
-
-def state_D(tape, cursor):
-    if tape[cursor] == 0:
-        tape[cursor] = 1
-        return 'A', cursor + 1
-    else:
-        tape[cursor] = 0
-        return 'B', cursor + 1
-
-
-def state_E(tape, cursor):
-    if tape[cursor] == 0:
-        tape[cursor] = 1
-        return 'F', cursor - 1
-    else:
-        tape[cursor] = 1
-        return 'C', cursor - 1
-
-
-def state_F(tape, cursor):
-    if tape[cursor] == 0:
-        tape[cursor] = 1
-        return 'D', cursor + 1
-    else:
-        tape[cursor] = 1
-        return 'A', cursor + 1
-
-
-def simulate(state, tape, cursor):
-    if state == 'A':
-        return state_A(tape, cursor)
-    elif state == 'B':
-        return state_B(tape, cursor)
-    elif state == 'C':
-        return state_C(tape, cursor)
-    elif state == 'D':
-        return state_D(tape, cursor)
-    elif state == 'E':
-        return state_E(tape, cursor)
-    elif state == 'F':
-        return state_F(tape, cursor)
+    return cursor + offset, new_state
 
 
 file_name = sys.argv[1]
@@ -80,20 +47,19 @@ with open(file_name) as f:
     
     begin_state = lines.pop(0)
     begin_state = begin_state[-3]
+
     nr_steps = lines.pop(0).strip().split(' ')
     nr_steps = int(nr_steps[-2])
 
-    print('begin', begin_state)
-    print('steps', nr_steps)
+    actions = parse(lines)
 
     tape = collections.defaultdict(int)
     cursor = 0
     state = begin_state
     for i in range(nr_steps):
         if i % 1000000 == 0:
-            print(i)
-        #print(tape)
-        state, cursor = simulate(state, tape, cursor)
+            print('step', i)
+        cursor, state = simulate(actions, state, tape, cursor)
 
     print('checksum:', sum(tape.values()))
 

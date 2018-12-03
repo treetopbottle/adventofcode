b98fd9e4518b9ef1f0482268ff8cbece636d8ebd
treetopbottle <treetopbottle@github>
2018-12-03 11:32:43 +0100

    Day 1 second star in Python

--- 2018/01/solution.py
+++ 2018/01/solution.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 
-result = 0
+current_frequency = 0
+seen_frequencies = set()
+
 
 with open('input') as f:
     lines = f.readlines()
-    for line in lines:
-        number = int(line[1:], 10)
-        if line[0] == '+':
-            result += number
-        else:
-            result -= number
+    def lines_loop():
+        while True:
+            for line in lines:
+                yield line
 
-print(result)
+    def get_change(line):
+        number = int(line[1:], 10)
+        if line[0] == '-':
+             return number * -1
+        return number
+
+    for line in lines_loop():
+        seen_frequencies.add(current_frequency)
+        current_frequency += get_change(line)
+        if current_frequency in seen_frequencies:
+            print(current_frequency)
+            break
+

27842e0fd7e3f8118b89e68bc01c127d062e292f
treetopbottle <treetopbottle@github>
2018-12-03 12:17:35 +0100

    Day 3 second star in Python

--- 2018/03/solution.py
+++ 2018/03/solution.py
@@ -11,7 +11,7 @@ def get_squares(claim):
     for i in range(x, x+width):
         for j in range(y, y+height):
             squares.append((i,j))
-    return squares
+    return id_, squares
 
 with open('input') as f:
     claims = f.readlines()
@@ -19,10 +19,19 @@ with open('input') as f:
     claimed_squares = set()
     overlapping_squares = set()
     for claim in claims:
-        squares = get_squares(claim)
+        id_, squares = get_squares(claim)
         for square in squares:
             if square in claimed_squares:
                 overlapping_squares.add(square)
             claimed_squares.add(square)
 
-    print(len(overlapping_squares))
+    for claim in claims:
+        id_, squares = get_squares(claim)
+        has_overlap = False
+        for square in squares:
+            if square in overlapping_squares:
+                has_overlap = True
+                continue
+        if not has_overlap:
+            print(id_)
+            break

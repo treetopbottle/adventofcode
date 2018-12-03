d899a5b43ec716f209bf83c9dba5d7fed4b4dd26
treetopbottle <treetopbottle@github>
2018-12-03 11:56:41 +0100

    Day 2 second star in Python

--- 2018/02/solution.py
+++ 2018/02/solution.py
@@ -2,19 +2,26 @@
 
 from collections import defaultdict
 
+def get_nr_different_characters(id1, id2):
+    different_characters = 0
+    for i in range(len(id1)):
+        if not id1[i] == id2[i]:
+            different_characters += 1
+    return different_characters
+
+def get_correct_box_ids(lines):
+    for box_id1 in lines:
+        for box_id2 in lines:
+            difference = get_nr_different_characters(box_id1, box_id2)
+            if difference == 1:
+                return [box_id1, box_id2]
+
 with open('input') as f:
     lines = f.readlines()
+    id1, id2 = get_correct_box_ids(lines)
 
-    two_occurences = 0
-    three_occurences = 0
-    for box_id in lines:
-        letter_occurences = defaultdict(int)
-        for letter in box_id:
-            letter_occurences[letter] += 1
-        occurences = letter_occurences.values()
-        if 2 in occurences:
-            two_occurences += 1
-        if 3 in occurences:
-            three_occurences += 1
-    print(two_occurences * three_occurences)
-        
+    # Print matching characters
+    for i in range(len(id1)):
+        if id1[i] == id2[i]:
+            print(id1[i], end='')
+

46faf8439a76c2ed09d104d9169f6d2eef9e1685
treetopbottle <treetopbottle@github>
2018-12-05 11:30:12 +0100

    Day 5 second star in Python

---
+++ 2018/05/python/star2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+
+with open('input') as f:
+    polymer = f.readlines()[0].strip()
+
+
+def react(polymer):
+    i = 0
+    while i < len(polymer) - 1:
+        unit1 = polymer[i]
+        unit2 = polymer[i+1]
+
+        different_case = \
+            (unit1.islower() and unit2.isupper()) \
+            or (unit1.isupper() and unit2.islower())
+
+        if different_case and (unit1.lower() == unit2.lower()):
+            polymer = polymer[:i] + polymer[i+2:]
+            i = max(i-2, -1)
+
+        i += 1
+
+    return polymer
+
+
+import string
+
+lengths = {}
+min_ = len(polymer)
+for lower in string.ascii_lowercase:
+    new_polymer = list(filter(lambda x: x.lower() != lower, polymer))
+    length_new = len(react(new_polymer))
+    min_ = min(min_, length_new)
+
+print(min_)

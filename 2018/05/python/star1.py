4846191077089f8af5ca14c5f9767e0d4dc0fad7
treetopbottle <treetopbottle@github>
2018-12-05 11:21:30 +0100

    Day 5 first star faster solution in Python

--- 2018/05/python/star1.py
+++ 2018/05/python/star1.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
 
 
-def react(polymer):
-    for i in range(len(polymer) - 1):
-        unit1 = polymer[i]
-        unit2 = polymer[i+1]
-        if (unit1.islower() and unit2.isupper()) \
-            or (unit1.isupper() and unit2.islower()):
-            if unit1.lower() == unit2.lower():
-                return polymer[:i] + polymer[i+2:]
-    return polymer
-
-
 with open('input') as f:
     polymer = f.readlines()[0].strip()
 
 
-old = None
-new = polymer
-while not old == new:
-    old = new
-    new = react(old)
+i = 0
+while i < len(polymer) - 1:
+    unit1 = polymer[i]
+    unit2 = polymer[i+1]
+
+    different_case = \
+        (unit1.islower() and unit2.isupper()) \
+        or (unit1.isupper() and unit2.islower())
+
+    if different_case and (unit1.lower() == unit2.lower()):
+        polymer = polymer[:i] + polymer[i+2:]
+        i = max(i-2, -1)
+
+    i += 1
 
 
-print(len(new))
+print(len(polymer))

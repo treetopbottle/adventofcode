3c752624e568a021c6768d580e845cdaf705695b
treetopbottle <treetopbottle@github>
2018-12-07 16:25:53 +0100

    Day 1 in Go use channel for second star

--- 2018/01-frequency-changes/go/star2.go
+++ 2018/01-frequency-changes/go/star2.go
@@ -7,25 +7,31 @@ import (
     "strconv"
 )
 
-func findDoubleFrequency(start int, changes []string) int {
-    frequency := start
-    seenFrequencies := map[int]int{frequency: 1}
+func loopInput(channel chan int) {
+    lines, _ := ioutil.ReadFile("input")
+    changes := strings.Split(string(lines[0:len(lines)-1]), "\n")
+
     for {
         for _, change := range changes {
             change, _ := strconv.Atoi(change)
-            frequency += change
-            seenFrequencies[frequency] += 1
-            if seenFrequencies[frequency] == 2 {
-                return frequency
-            }
+            channel <- change
         }
     }
 }
 
 func main() {
-    lines, _ := ioutil.ReadFile("input")
-    changes := strings.Split(string(lines[0:len(lines)-1]), "\n")
+    changesLoop := make(chan int, 2)
+    go loopInput(changesLoop)
 
-    doubleFrequency := findDoubleFrequency(0, changes)
-    fmt.Println(doubleFrequency)
+    frequency := 0
+    seenFrequencies := map[int]int{frequency: 1}
+    for {
+        change := <-changesLoop
+        frequency += change
+        seenFrequencies[frequency] += 1
+        if seenFrequencies[frequency] == 2 {
+            fmt.Println(frequency)
+            break
+        }
+    }
 }

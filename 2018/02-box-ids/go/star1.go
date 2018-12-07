7d9dc5d0a475d7de4ce1050fe2fae5fedc2b7088
treetopbottle <treetopbottle@github>
2018-12-07 18:06:35 +0100

    Day 2 in Go

---
+++ 2018/02-box-ids/go/star1.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "bytes"
+    "strings"
+)
+
+func main() {
+    lines, _ := ioutil.ReadFile("input")
+    boxIds := []string{}
+    for _, line := range bytes.Split(lines, []byte{'\n'}) {
+        if len(line) > 0 {
+            boxIds = append(boxIds, string(line))
+        }
+    }
+
+    numberTwos := 0
+    numberThrees := 0
+
+    for _, boxId := range boxIds {
+        twoFound := false
+        threeFound := false
+
+        for _, char := range boxId {
+            if strings.Count(boxId, string(char)) == 2 {
+                twoFound = true
+            }
+            if strings.Count(boxId, string(char)) == 3 {
+                threeFound = true
+            }
+        }
+
+        if twoFound {
+            numberTwos += 1
+        }
+        if threeFound {
+            numberThrees += 1
+        }
+    }
+
+    fmt.Println(numberTwos * numberThrees)
+}
---
+++ 2018/02-box-ids/go/star2.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "bytes"
+)
+
+func main() {
+    lines, _ := ioutil.ReadFile("input")
+    boxIds := []string{}
+    for _, line := range bytes.Split(lines, []byte{'\n'}) {
+        if len(line) > 0 {
+            boxIds = append(boxIds, string(line))
+        }
+    }
+
+    for _, boxId1 := range boxIds {
+        for _, boxId2 := range boxIds {
+            differences := 0
+            for i := 0; i < len(boxId1); i++ {
+                if boxId1[i] != boxId2[i] {
+                    differences += 1
+                }
+            }
+            if differences == 1 {
+                for i := 0; i < len(boxId1); i++ {
+                    if boxId1[i] == boxId2[i] {
+                        fmt.Print(string(boxId1[i]))
+                    }
+                }
+                fmt.Println()
+            }
+        }
+    }
+}

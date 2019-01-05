006848a9fba8e2c84ed2b7a5bd6802162b0eb99b
treetopbottle <treetopbottle@github>
2019-01-05 03:51:44 +0100

    Day 11 star 2 in Python better solution

--- 2018/11-power-grid/python/star2.py
+++ 2018/11-power-grid/python/star2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 
+from collections import defaultdict
+
+
 def get_power(x, y, serial_number):
     rack_id = x + 10
     power_level = rack_id * y
@@ -14,22 +17,28 @@ def get_power(x, y, serial_number):
 grid_serial_number = 8444
 
 fuel_cell_grid = {}
-for x in range(1,301):
-    for y in range(1,301):
+summed_area_table = defaultdict(int)
+for x in range(1, 301):
+    for y in range(1, 301):
         fuel_cell_grid[(x, y)] = get_power(x, y, grid_serial_number)
+        summed_area_table[(x, y)] = fuel_cell_grid[(x, y)] + \
+            summed_area_table[(x-1, y  )] + \
+            summed_area_table[(x  , y-1)] - \
+            summed_area_table[(x-1, y-1)]
+
 
 topleftsize = (-1, -1, -1)
 highest = -1000
-for s in range(10, 20):
-    for x in range(1, 302-s):
-        for y in range(1, 302-s):
-            square_power = 0
-            for i in range(0,s):
-                for j in range(0,s):
-                    square_power += fuel_cell_grid[(x+i, y+j)]
+for s in range(1, 301):
+    for x in range(s, 301):
+        for y in range(s, 301):
+            square_power = summed_area_table[(x, y)] + \
+                summed_area_table[(x-s, y-s)] - \
+                summed_area_table[(x-s, y  )] - \
+                summed_area_table[(x  , y-s)]
             if square_power > highest:
                 highest = square_power
-                topleftsize = (x, y, s)
+                topleftsize = (x-s+1, y-s+1, s)
 
 print(highest)
 print(topleftsize)

b3c93f64f1f75ec92a6ee31826a1319cb6fce725
treetopbottle <treetopbottle@github>
2019-01-08 17:30:10 +0100

    Day 14 in Python simplify star1.py

--- 2018/14-elf-cooking/python/star1.py
+++ 2018/14-elf-cooking/python/star1.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python3
 
-NR_RECIPES_FOR_SCORE = 10
+SCORE_LEN = 10
 
 with open('input') as f:
-    nr_recipes_before_score = int(f.read().strip())
+    nr_recipes = int(f.read().strip())
 
-recipe_board = [3, 7]
+recipe_board = '37'
 elf1 = 0
 elf2 = 1
 
-desired_board_length = (nr_recipes_before_score + NR_RECIPES_FOR_SCORE)
-while len(recipe_board) < desired_board_length:
-    new_score = recipe_board[elf1] + recipe_board[elf2]
-    new_recipes = [int(s) for s in list(str(new_score))]
-    recipe_board.extend(new_recipes)
-    elf1 = (elf1 + 1 + recipe_board[elf1]) % len(recipe_board)
-    elf2 = (elf2 + 1 + recipe_board[elf2]) % len(recipe_board)
+while len(recipe_board) < nr_recipes + SCORE_LEN:
+    new_score = int(recipe_board[elf1]) + int(recipe_board[elf2])
+    recipe_board += str(new_score)
+    elf1 = (elf1 + 1 + int(recipe_board[elf1])) % len(recipe_board)
+    elf2 = (elf2 + 1 + int(recipe_board[elf2])) % len(recipe_board)
     assert elf1 != elf2
 
-start = nr_recipes_before_score
-end = nr_recipes_before_score+NR_RECIPES_FOR_SCORE
-str_scores = [str(i) for i in recipe_board[start:end]]
-print(''.join(str_scores))
+print(recipe_board[nr_recipes:nr_recipes+SCORE_LEN])

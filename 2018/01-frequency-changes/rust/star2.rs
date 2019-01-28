0277c05378470c56e123e7ce6d82d176bc9afe41
treetopbottle <treetopbottle@github>
2019-01-28 17:18:30 +0100

    Day 1 in Rust shorten star2.rs

--- 2018/01-frequency-changes/rust/star2.rs
+++ 2018/01-frequency-changes/rust/star2.rs
@@ -1,19 +1,18 @@
 use std::collections::HashSet;
 
-fn main() {
-    let input = include_str!("../input");
+const INPUT: &str = include_str!("../input");
 
+fn main() {
     let mut frequency = 0;
     let mut seen_frequencies = HashSet::new();
+
     'outer: loop {
-        for line in input.lines() {
-            let change = line.parse::<i32>().unwrap();
+        for change in INPUT.lines().map(|s| s.parse::<i32>().unwrap()) {
             frequency += change;
-            if seen_frequencies.contains(&frequency) {
+            if !seen_frequencies.insert(frequency) {
                 println!("{}", frequency);
                 break 'outer;
             }
-            seen_frequencies.insert(frequency);
         }
     }
 }

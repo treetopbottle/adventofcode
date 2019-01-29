260fd0eae1e8301cb08cb956be50eadd31ccd59e
treetopbottle <treetopbottle@github>
2019-01-29 12:04:32 +0100

    Day 2 in Rust use filter and map for star 2

--- 2018/02-box-ids/rust/star2.rs
+++ 2018/02-box-ids/rust/star2.rs
@@ -1,24 +1,17 @@
 const INPUT: &str = include_str!("../input");
 
 fn main() {
-    'outer: for id1 in INPUT.lines() {
+    for id1 in INPUT.lines() {
         for id2 in INPUT.lines() {
-            let mut different_chars = 0;
-            for (c1, c2) in id1.chars().zip(id2.chars()) {
-                if c1 != c2 {
-                    different_chars += 1;
-                }
-            }
+            let same_chars: String = id1
+                .chars()
+                .zip(id2.chars())
+                .filter(|(c1, c2)| c1 == c2)
+                .map(|(c1, _)| c1)
+                .collect();
 
-            if different_chars == 1 {
-                for (c1, c2) in id1.chars().zip(id2.chars()) {
-                    if c1 == c2 {
-                        print!("{}", c1);
-                        different_chars += 1;
-                    }
-                }
-                println!();
-                break 'outer;
+            if same_chars.len() == id1.len() - 1 {
+                println!("{}", same_chars);
             }
         }
     }

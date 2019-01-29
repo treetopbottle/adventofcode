5710ead83f86b4de566e5859a8ce38677a7a442b
treetopbottle <treetopbottle@github>
2019-01-29 12:02:37 +0100

    Day 2 in Rust use zip for star 2

--- 2018/02-box-ids/rust/star2.rs
+++ 2018/02-box-ids/rust/star2.rs
@@ -4,22 +4,17 @@ fn main() {
     'outer: for id1 in INPUT.lines() {
         for id2 in INPUT.lines() {
             let mut different_chars = 0;
-            // Direct indexing in a string isn't supported, so loop over both
-            for (i, c1) in id1.char_indices() {
-                for (j, c2) in id2.char_indices() {
-                    if i == j && c1 != c2 {
-                        different_chars += 1
-                    }
+            for (c1, c2) in id1.chars().zip(id2.chars()) {
+                if c1 != c2 {
+                    different_chars += 1;
                 }
             }
 
             if different_chars == 1 {
-                // Print the matching characters
-                for (i, c1) in id1.char_indices() {
-                    for (j, c2) in id2.char_indices() {
-                        if i == j && c1 == c2 {
-                            print!("{}", c1);
-                        }
+                for (c1, c2) in id1.chars().zip(id2.chars()) {
+                    if c1 == c2 {
+                        print!("{}", c1);
+                        different_chars += 1;
                     }
                 }
                 println!();

e1bb6711cec8f1b9f32ac08d80badeb0bb0d76af
treetopbottle <treetopbottle@github>
2019-02-26 21:34:19 +0100

    Day 6 in Rust cleanup

--- 2018/06-nearest-neighbour-coordinates/rust/star1.rs
+++ 2018/06-nearest-neighbour-coordinates/rust/star1.rs
@@ -1,57 +1,45 @@
-use std::cmp::min;
-use std::cmp::max;
 use std::collections::HashMap;
 
 const INPUT: &str = include_str!("../input");
 
 fn distance(point1: (i32, i32), point2: (i32, i32)) -> i32 {
-    return (point1.0 - point2.0).abs() + (point1.1 - point2.1).abs()
+    (point1.0 - point2.0).abs() + (point1.1 - point2.1).abs()
+}
+
+fn parse_line(line: &str) -> (i32, i32) {
+    let ints: Vec<i32> = line.split(", ").map(|s| s.parse().unwrap()).collect();
+
+    (ints[0], ints[1])
 }
 
 fn main() {
-    let mut top_left = (i32::max_value(), i32::max_value());
-    let mut bottom_right = (i32::min_value(), i32::min_value());
-    let mut coordinates: Vec<(i32, i32)> = Vec::new();
-    for line in INPUT.lines() {
-        let ints: Vec<i32> = line.split(", ")
-            .map(|s| s.parse()
-            .unwrap())
-            .collect();
+    let coordinates: Vec<(i32, i32)> = INPUT.lines().map(|l| parse_line(l)).collect();
 
-        top_left = (
-            min(top_left.0, ints[0]),
-            min(top_left.1, ints[1])
-        );
-        bottom_right = (
-            max(bottom_right.0, ints[0]),
-            max(bottom_right.1, ints[1])
-        );
-        coordinates.push((ints[0], ints[1]));
-    }
+    let top_left = (
+        coordinates.iter().map(|c| c.0).min().unwrap(),
+        coordinates.iter().map(|c| c.1).min().unwrap(),
+    );
+    let bottom_right = (
+        coordinates.iter().map(|c| c.0).max().unwrap(),
+        coordinates.iter().map(|c| c.1).max().unwrap(),
+    );
 
-    let mut largest_area = i32::min_value();
     let mut areas: HashMap<(i32, i32), i32> = HashMap::new();
     for x in (top_left.0 + 1)..(bottom_right.0 - 1) {
         for y in (top_left.1 + 1)..(bottom_right.1 - 1) {
-            let mut min_distance = i32::max_value();
-            let mut closests: Vec<(i32, i32)> = Vec::new();
-            for coordinate in coordinates.as_slice() {
-                if distance(*coordinate, (x, y)) < min_distance {
-                    min_distance = distance(*coordinate, (x, y));
-                    closests = Vec::new();
-                    closests.push(*coordinate);
-                } else if distance(*coordinate, (x, y)) == min_distance {
-                    min_distance = distance(*coordinate, (x, y));
-                    closests.push(*coordinate);
-                }
-            }
-            if closests.len() == 1 {
-                let closest = closests[0];
+            let mut distances: Vec<(i32, (i32, i32))> = coordinates
+                .iter()
+                .map(|c| (distance(*c, (x, y)), *c))
+                .collect();
+
+            distances.sort();
+            if distances[0].0 != distances[1].0 {
+                let closest = distances[0].1;
                 *areas.entry(closest).or_default() += 1;
-                largest_area = max(largest_area, areas[&closest]);
             }
         }
     }
 
+    let largest_area = areas.values().max().unwrap();
     println!("{}", largest_area);
 }
--- 2018/06-nearest-neighbour-coordinates/rust/star2.rs
+++ 2018/06-nearest-neighbour-coordinates/rust/star2.rs
@@ -1,41 +1,33 @@
-use std::cmp::min;
-use std::cmp::max;
-
 const INPUT: &str = include_str!("../input");
 
 fn distance(point1: (i32, i32), point2: (i32, i32)) -> i32 {
-    return (point1.0 - point2.0).abs() + (point1.1 - point2.1).abs()
+    (point1.0 - point2.0).abs() + (point1.1 - point2.1).abs()
+}
+
+fn parse_line(line: &str) -> (i32, i32) {
+    let ints: Vec<i32> = line.split(", ").map(|s| s.parse().unwrap()).collect();
+
+    (ints[0], ints[1])
 }
 
 fn main() {
-    let mut top_left = (i32::max_value(), i32::max_value());
-    let mut bottom_right = (i32::min_value(), i32::min_value());
-    let mut coordinates: Vec<(i32, i32)> = Vec::new();
-    for line in INPUT.lines() {
-        let ints: Vec<i32> = line.split(", ")
-            .map(|s| s.parse()
-            .unwrap())
-            .collect();
+    let coordinates: Vec<(i32, i32)> = INPUT.lines().map(|l| parse_line(l)).collect();
 
-        top_left = (
-            min(top_left.0, ints[0]),
-            min(top_left.1, ints[1])
-        );
-        bottom_right = (
-            max(bottom_right.0, ints[0]),
-            max(bottom_right.1, ints[1])
-        );
-        coordinates.push((ints[0], ints[1]));
-    }
+    let top_left = (
+        coordinates.iter().map(|c| c.0).min().unwrap(),
+        coordinates.iter().map(|c| c.1).min().unwrap(),
+    );
+    let bottom_right = (
+        coordinates.iter().map(|c| c.0).max().unwrap(),
+        coordinates.iter().map(|c| c.1).max().unwrap(),
+    );
 
     const MAX_DISTANCE: i32 = 10000;
     let mut region_size = 0;
     for x in (top_left.0)..(bottom_right.0) {
         for y in (top_left.1)..(bottom_right.1) {
-            let mut total_distance = 0;
-            for coordinate in coordinates.as_slice() {
-                total_distance += distance(*coordinate, (x, y));
-            }
+            let total_distance: i32 = coordinates.iter().map(|c| distance(*c, (x, y))).sum();
+
             if total_distance < MAX_DISTANCE {
                 region_size += 1;
             }
